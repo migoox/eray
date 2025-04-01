dab6a36e77dc906f3a740ac095dac61d9a11b7d0
Michał Okurowski <micokoku@gmail.com>
2025-04-01 02:52:16 +0200

    Add quat cast

--- liberay/liberay/math/quat.hpp
+++ liberay/liberay/math/quat.hpp
@@ -3,6 +3,8 @@
 #include <liberay/math/mat.hpp>
 #include <liberay/math/quat_fwd.hpp>
 
+#include "liberay/math/mat_fwd.hpp"
+
 namespace eray::math {
 
 template <CFloatingPoint T>
@@ -84,6 +86,48 @@ struct Quat {
         static_cast<T>(0),
         std::sin(rad_angle / static_cast<T>(2)),
     };
+  }
+
+  static Quat from_rotation_mat(const Mat<4, 4, T>& m) {
+    // From https://github.com/g-truc/glm/blob/2d4c4b4dd31fde06cfffad7915c2b3006402322f/glm/gtc/quaternion.inl#L81
+    // NOLINTBEGIN
+    T fourXSquaredMinus1 = m[0][0] - m[1][1] - m[2][2];
+    T fourYSquaredMinus1 = m[1][1] - m[0][0] - m[2][2];
+    T fourZSquaredMinus1 = m[2][2] - m[0][0] - m[1][1];
+    T fourWSquaredMinus1 = m[0][0] + m[1][1] + m[2][2];
+
+    int biggestIndex           = 0;
+    T fourBiggestSquaredMinus1 = fourWSquaredMinus1;
+    if (fourXSquaredMinus1 > fourBiggestSquaredMinus1) {
+      fourBiggestSquaredMinus1 = fourXSquaredMinus1;
+      biggestIndex             = 1;
+    }
+    if (fourYSquaredMinus1 > fourBiggestSquaredMinus1) {
+      fourBiggestSquaredMinus1 = fourYSquaredMinus1;
+      biggestIndex             = 2;
+    }
+    if (fourZSquaredMinus1 > fourBiggestSquaredMinus1) {
+      fourBiggestSquaredMinus1 = fourZSquaredMinus1;
+      biggestIndex             = 3;
+    }
+
+    T biggestVal = sqrt(fourBiggestSquaredMinus1 + static_cast<T>(1)) * static_cast<T>(0.5);
+    T mult       = static_cast<T>(0.25) / biggestVal;
+
+    switch (biggestIndex) {
+      case 0:
+        return Quat{biggestVal, (m[1][2] - m[2][1]) * mult, (m[2][0] - m[0][2]) * mult, (m[0][1] - m[1][0]) * mult};
+      case 1:
+        return Quat{(m[1][2] - m[2][1]) * mult, biggestVal, (m[0][1] + m[1][0]) * mult, (m[2][0] + m[0][2]) * mult};
+      case 2:
+        return Quat{(m[2][0] - m[0][2]) * mult, (m[0][1] + m[1][0]) * mult, biggestVal, (m[1][2] + m[2][1]) * mult};
+      case 3:
+        return Quat{(m[0][1] - m[1][0]) * mult, (m[2][0] + m[0][2]) * mult, (m[1][2] + m[2][1]) * mult, biggestVal};
+      default:  // Silence a -Wswitch-default warning in GCC. Should never actually get here. Assert is just for sanity.
+        assert(false);
+        return Quat{1, 0, 0, 0};
+    }
+    // NOLINTEND
   }
 
   /**
