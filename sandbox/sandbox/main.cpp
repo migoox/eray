12d159df61c0b35f50a9940719c474d6be930f94
Michał Okurowski <micokoku@gmail.com>
2025-07-22 15:22:12 +0200

    VkTriangle: ValidationLayers

--- sandbox/sandbox/main.cpp
+++ sandbox/sandbox/main.cpp
@@ -1,22 +1,34 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan_core.h>
 
+#include <algorithm>
 #include <expected>
 #include <liberay/util/logger.hpp>
 #include <liberay/util/panic.hpp>
 #include <liberay/util/try.hpp>
+#include <liberay/util/zstring_view.hpp>
 #include <variant>
 #include <version/version.hpp>
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_raii.hpp>
+#include <vulkan/vulkan_structs.hpp>
+#include <vulkan/vulkan_to_string.hpp>
 
-struct GLFWExtensionNotSupportedError {
+struct VulkanExtensionNotSupported {
   std::string glfw_extension;
 };
-struct VulkanInstanceCreationFailed {};
+struct SomeOfTheRequestedVulkanLayersAreNotSupported {};
+struct VulkanInstanceCreationFailure {
+  vk::Result result;
+};
+struct VulkanDebugMessengerCreationFailure {
+  vk::Result result;
+};
 
-using VulkanInitError = std::variant<GLFWExtensionNotSupportedError, VulkanInstanceCreationFailed>;
-using AppError        = std::variant<VulkanInitError>;
+using VulkanInitError =
+    std::variant<VulkanExtensionNotSupported, VulkanInstanceCreationFailure,
+                 SomeOfTheRequestedVulkanLayersAreNotSupported, VulkanDebugMessengerCreationFailure>;
+using AppError = std::variant<VulkanInitError>;
 
 class HelloTriangleApplication {
  public:
@@ -33,6 +45,7 @@ class HelloTriangleApplication {
  private:
   std::expected<void, VulkanInitError> initVk() {
     TRY(createVkInstance())
+    TRY(setup_debug_messenger())
 
     return {};
   }
@@ -44,6 +57,14 @@ class HelloTriangleApplication {
   void cleanup() { eray::util::Logger::succ("Finished cleanup"); }
 
   std::expected<void, VulkanInitError> createVkInstance() {
+    // To create a Vulkan Instance we specify
+    //  1. app info
+    //  2. global extensions (e.g. those needed by GLFW)
+    //  3. validation layers (by default vulkan has no overhead, when debugging it's useful to do runtime checks if api
+    //  is used correctly)
+
+    // == 1. App info ==================================================================================================
+
     // Technically optional
     auto app_info = vk::ApplicationInfo{
         .pApplicationName   = "Hello Triangle",
@@ -53,37 +74,49 @@ class HelloTriangleApplication {
         .apiVersion         = vk::ApiVersion14  //
     };
 
-    // Wrapper over vkEnumerateInstanceExtensionProperties. Returns up to requested number of global extension
-    // properties.
-    // see: https://registry.khronos.org/vulkan/specs/latest/man/html/vkEnumerateInstanceExtensionProperties.html
+    // == 2. Global Extensions =========================================================================================
+
+    // Wrapper over vkEnumerateInstanceExtensionProperties. Returns global extension properties.
+    // See: https://registry.khronos.org/vulkan/specs/latest/man/html/vkEnumerateInstanceExtensionProperties.html
     auto extension_props = context_.enumerateInstanceExtensionProperties();
 
-    // GLFW has a function that returns Vulkan extension(s) that are needed to integrate GLFW with Vulkan
-    uint32_t glfw_extensions_count = 0;
-    auto* glfw_extensions          = glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
-
     // Check if the required GLFW extensions are supported by the Vulkan implementation.
-    for (auto i = 0U; i < glfw_extensions_count; ++i) {
-      if (std::ranges::none_of(extension_props,
-                               [ext_name = std::string_view(glfw_extensions[i])](const vk::ExtensionProperties& prop) {
-                                 return std::string_view(prop.extensionName) == ext_name;
-                               })) {
-        eray::util::Logger::err("Required GLFW extension not supported: {}", glfw_extensions[i]);
-        return std::unexpected(GLFWExtensionNotSupportedError{
-            .glfw_extension = std::string(glfw_extensions[i])  //
+    auto required_extensions = get_required_extensions();
+    for (const auto& ext : required_extensions) {
+      if (std::ranges::none_of(extension_props, [ext_name = std::string_view(ext)](const auto& prop) {
+            return std::string_view(prop.extensionName) == ext_name;
+          })) {
+        eray::util::Logger::err("Required GLFW extension not supported: {}", ext);
+        return std::unexpected(VulkanExtensionNotSupported{
+            .glfw_extension = std::string(ext)  //
         });
       }
     }
 
-    // This struct is NOT optional. It allows to specify:
-    //  - global extensions (e.g. those used by glfw)
-    //  - validation layers (by default vulkan has no overhead, when debugging it's useful to do runtime checks if api
-    //  is used correctly)
-    //  - app info
+    // == 3. Validation Layers =========================================================================================
+
+    auto required_layers = get_required_validation_layers();
+    auto layer_props     = context_.enumerateInstanceLayerProperties();
+    if (std::ranges::any_of(required_layers, [&layer_props](const auto& required_layer) {
+          return std::ranges::none_of(layer_props, [required_layer](auto const& layer_prop) {
+            return required_layer == std::string_view(layer_prop.layerName);
+          });
+        })) {
+      eray::util::Logger::err("Failed to create a vulkan instance. Use of unsupported validation layer(s).");
+      return std::unexpected(SomeOfTheRequestedVulkanLayersAreNotSupported());
+    }
+
+    // == Vulkan Instance Creation =====================================================================================
+
     auto create_info = vk::InstanceCreateInfo{
-        .pApplicationInfo        = &app_info,
-        .enabledExtensionCount   = glfw_extensions_count,
-        .ppEnabledExtensionNames = glfw_extensions,
+        // app info
+        .pApplicationInfo = &app_info,
+        // validation layers
+        .enabledLayerCount   = static_cast<uint32_t>(required_layers.size()),
+        .ppEnabledLayerNames = required_layers.data(),
+        // global extensions
+        .enabledExtensionCount   = static_cast<uint32_t>(required_extensions.size()),
+        .ppEnabledExtensionNames = required_extensions.data(),  //
     };
 
     // When using VULKAN_HPP_NO_EXCEPTIONS, the vk::raii::Instance(context_, create_info) is unavailable,
@@ -92,12 +125,89 @@ class HelloTriangleApplication {
     if (auto instance_opt = context_.createInstance(create_info)) {
       instance_ = std::move(instance_opt.value());
     } else {
-      eray::util::Logger::err("Failed to create a vulkan instance.");
-      return std::unexpected(VulkanInitError{});
+      auto err = vk::to_string(instance_opt.error());
+      eray::util::Logger::err("Failed to create a vulkan instance. Error type: {}", err);
+      return std::unexpected(VulkanInstanceCreationFailure{.result = instance_opt.error()});
     }
 
     eray::util::Logger::succ("Vulkan Instance has been created");
+
     return {};
+  }
+
+  std::expected<void, VulkanDebugMessengerCreationFailure> setup_debug_messenger() {
+    if (!kEnableValidationLayers) {
+      return {};
+    }
+    auto severity_flags = vk::DebugUtilsMessageSeverityFlagsEXT(vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo |
+                                                                vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                                                                vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+    auto msg_type_flags = vk::DebugUtilsMessageTypeFlagsEXT(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                                                            vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+                                                            vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+
+    auto debug_utils_messenger_create_info_ext = vk::DebugUtilsMessengerCreateInfoEXT{
+        .messageSeverity = severity_flags,
+        .messageType     = msg_type_flags,
+        .pfnUserCallback = &debug_callback  //
+    };
+
+    if (auto debug_messenger_opt = instance_.createDebugUtilsMessengerEXT(debug_utils_messenger_create_info_ext)) {
+      debug_messenger_ = std::move(debug_messenger_opt.value());
+    } else {
+      auto err = vk::to_string(debug_messenger_opt.error());
+      eray::util::Logger::err("Failed to create a vulkan debug messenger. Error type: {}", err);
+      return std::unexpected(VulkanDebugMessengerCreationFailure{
+          .result = debug_messenger_opt.error()  //
+      });
+    }
+
+    return {};
+  }
+
+  std::vector<const char*> get_required_extensions() {
+    // GLFW has a function that returns Vulkan extension(s) that are needed to integrate GLFW with Vulkan
+    uint32_t glfw_extensions_count = 0;
+    auto* glfw_extensions          = glfwGetRequiredInstanceExtensions(&glfw_extensions_count);
+
+    auto required_extensions = std::vector<const char*>(glfw_extensions, glfw_extensions + glfw_extensions_count);
+    if (kEnableValidationLayers) {
+      // The extension is needed to setup a debug messenger
+      required_extensions.push_back(
+          vk::EXTDebugUtilsExtensionName  // = VK_EXT_DEBUG_UTILS_EXTENSION_NAME = "VK_EXT_debug_utils"
+      );
+    }
+
+    return required_extensions;
+  }
+
+  std::vector<const char*> get_required_validation_layers() {
+    auto required_layers = std::vector<const char*>();
+    if (kEnableValidationLayers) {
+      eray::util::Logger::info("Vulkan validation layers are enabled");
+      required_layers.assign(kValidationLayers.begin(), kValidationLayers.end());
+    }
+
+    return required_layers;
+  }
+
+  static VKAPI_ATTR vk::Bool32 VKAPI_CALL debug_callback(vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
+                                                         vk::DebugUtilsMessageTypeFlagsEXT type,
+                                                         const vk::DebugUtilsMessengerCallbackDataEXT* p_callback_data,
+                                                         void*) {
+    switch (severity) {
+      case vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo:
+        eray::util::Logger::info("Vulkan Debug (Type: {}): {}", vk::to_string(type), p_callback_data->pMessage);
+        return vk::True;
+      case vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning:
+        eray::util::Logger::warn("Vulkan Debug (Type: {}): {}", vk::to_string(type), p_callback_data->pMessage);
+        return vk::True;
+      case vk::DebugUtilsMessageSeverityFlagBitsEXT::eError:
+        eray::util::Logger::err("Vulkan Debug (Type: {}): {}", vk::to_string(type), p_callback_data->pMessage);
+        return vk::True;
+      default:
+        return vk::False;
+    };
   }
 
  private:
@@ -114,6 +224,31 @@ class HelloTriangleApplication {
    *
    */
   vk::raii::Instance instance_ = nullptr;
+
+  /**
+   * @brief When VK_EXT_debug_utils is used, debug_messenger allows to set debug callback to integrate the Vulkan API
+   * with logger.
+   *
+   */
+  vk::raii::DebugUtilsMessengerEXT debug_messenger_ = nullptr;
+
+  /**
+   * @brief Validation layers are optional components that hook into Vulkan function calls to apply additional
+   * operations. Common operations in validation layers are:
+   *  - Checking the values of parameters against the specification to detect misuse
+   *  - Tracking the creation and destruction of objects to find resource leaks
+   *  - Checking thread safety by tracking the threads that calls originate from
+   *  - Logging every call and its parameters to the standard output
+   *  - Tracing Vulkan calls for profiling and replaying
+   *
+   */
+#ifdef NDEBUG
+  static constexpr bool kEnableValidationLayers = false;
+#else
+  static constexpr bool kEnableValidationLayers = true;
+#endif
+
+  static constexpr std::array<const char*, 1> kValidationLayers = {"VK_LAYER_KHRONOS_validation"};
 };
 
 int main() {
